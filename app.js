69facf521f2aacb2610584d5c39dab1a297e4306
amina-work <mina.works28@gmail.com>
2023-01-24 10:32:28 +0100

    step2 prog

--- app.js
+++ app.js
@@ -14,10 +14,13 @@ function checkName() {
   
     if (myNameInput.value.trim() == "") {
         myNameError.innerHTML = "This field is required";
+        myNameInput.style.border = "1px solid hsl(354, 84%, 57%)";
     } else if (myNameRegex.test(myNameInput.value) == false) {
         myNameError.innerHTML = "Can't contain numbers or symbols";
+        myNameInput.style.border = "1px solid hsl(354, 84%, 57%)";
     } else if (myNameInput.value.trim() !== "") {
         myNameError.innerHTML = "";
+        myNameInput.style.border = "1px solid hsl(243, 100%, 62%)";
     }
 }
   
@@ -28,26 +31,33 @@ function checkNum() {
   
     if (myNumInput.value.trim() == "") {
         myNumError.innerHTML = "This field is required";
+        myNumInput.style.border = "1px solid hsl(354, 84%, 57%)";
     } else if (myNumRegex.test(myNumInput.value) == false) {
         myNumError.innerHTML = "Must contain number";
+        myNumInput.style.border = "1px solid hsl(354, 84%, 57%)";
     } else if (myNumInput.value.trim().length !== 10) {
         myNumError.innerHTML = "Invalid Phone Number";
+        myNumInput.style.border = "1px solid hsl(354, 84%, 57%)";
     } else {
         myNumError.innerHTML = "";
+        myNumInput.style.border = "1px solid hsl(243, 100%, 62%)";
     }
 }
   
 function checkEmail() {
-    let myMailInput = document.getElementById("email");
-    let myNumError = document.getElementById("error-email");
-    let myMailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let myEmailInput = document.getElementById("email");
+    let myEmailError = document.getElementById("error-email");
+    let myEmailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   
-    if (myMailInput.value.trim() == "") {
-        myNumError.innerHTML = "This field is required";
-    } else if (myMailRegex.test(myMailInput.value) == false) {
-        myNumError.innerHTML = "Please enter a valid e-mail";
+    if (myEmailInput.value.trim() == "") {
+        myEmailError.innerHTML = "This field is required";
+        myEmailInput.style.border = "1px solid hsl(354, 84%, 57%)";
+    } else if (myEmailRegex.test(myEmailInput.value) == false) {
+        myEmailError.innerHTML = "Please enter a valid e-mail";
+        myEmailInput.style.border = "1px solid hsl(354, 84%, 57%)";
     } else {
-        myNumError.innerHTML = "";
+        myEmailError.innerHTML = "";
+        myEmailInput.style.border = "1px solid hsl(243, 100%, 62%)";
     }
 }
   
@@ -90,4 +100,121 @@ checkBox = document.getElementById("toggle").addEventListener("click", event => 
         addonAnnee.style.display = "none";
         addonMois.style.display = "flex";
     }
+});
+/* MONTHLY Choosing a plan / changing colors */
+checkBox = document.getElementById("monthlyPlanArc").addEventListener("click", event => {
+    monthlyPlanArc.style.backgroundColor = "hsl(217, 100%, 97%)";
+    monthlyPlanArc.style.border = " solid 1px hsl(243, 100%, 62%)";
+    /*res les autres background et borders */
+    monthlyPlanAdv.style.backgroundColor = "white";
+    monthlyPlanAdv.style.border = " solid 1px hsl(231, 11%, 63%)";
+    monthlyPlanPro.style.backgroundColor = "white";
+    monthlyPlanPro.style.border = " solid 1px hsl(231, 11%, 63%)";
+    yearlyPlanArc.style.backgroundColor = "white";
+    yearlyPlanArc.style.border = " solid 1px hsl(231, 11%, 63%)";
+    yearlyPlanAdv.style.backgroundColor = "white";
+    yearlyPlanAdv.style.border = " solid 1px hsl(231, 11%, 63%)";
+    yearlyPlanPro.style.backgroundColor = "white";
+    yearlyPlanPro.style.border = " solid 1px hsl(231, 11%, 63%)";
+    /* la suite */
+    //document.getElementById("modeResume").innerHTML = "Arcade (monthly)";
+    //document.getElementById("priceResume").innerHTML = "9$/mo";
+    //document.getElementById("modeTotal").innerHTML = "Total (per month)";
+});
+checkBox = document.getElementById("monthlyPlanAdv").addEventListener("click", event => {
+    monthlyPlanAdv.style.backgroundColor = "hsl(217, 100%, 97%)";
+    monthlyPlanAdv.style.border = " solid 1px hsl(243, 100%, 62%)";
+    /*res les autres background et borders */
+    monthlyPlanArc.style.backgroundColor = "white";
+    monthlyPlanArc.style.border = " solid 1px hsl(231, 11%, 63%)";
+    monthlyPlanPro.style.backgroundColor = "white";
+    monthlyPlanPro.style.border = " solid 1px hsl(231, 11%, 63%)";
+    yearlyPlanArc.style.backgroundColor = "white";
+    yearlyPlanArc.style.border = " solid 1px hsl(231, 11%, 63%)";
+    yearlyPlanAdv.style.backgroundColor = "white";
+    yearlyPlanAdv.style.border = " solid 1px hsl(231, 11%, 63%)";
+    yearlyPlanPro.style.backgroundColor = "white";
+    yearlyPlanPro.style.border = " solid 1px hsl(231, 11%, 63%)";
+    /* la suite */
+    //document.getElementById("modeResume").innerHTML = "Arcade (monthly)";
+    //document.getElementById("priceResume").innerHTML = "9$/mo";
+    //document.getElementById("modeTotal").innerHTML = "Total (per month)";
+});
+checkBox = document.getElementById("monthlyPlanPro").addEventListener("click", event => {
+    monthlyPlanPro.style.backgroundColor = "hsl(217, 100%, 97%)";
+    monthlyPlanPro.style.border = " solid 1px hsl(243, 100%, 62%)";
+    /*res les autres background et borders */
+    monthlyPlanAdv.style.backgroundColor = "white";
+    monthlyPlanAdv.style.border = " solid 1px hsl(231, 11%, 63%)";
+    monthlyPlanArc.style.backgroundColor = "white";
+    monthlyPlanArc.style.border = " solid 1px hsl(231, 11%, 63%)";
+    yearlyPlanArc.style.backgroundColor = "white";
+    yearlyPlanArc.style.border = " solid 1px hsl(231, 11%, 63%)";
+    yearlyPlanAdv.style.backgroundColor = "white";
+    yearlyPlanAdv.style.border = " solid 1px hsl(231, 11%, 63%)";
+    yearlyPlanPro.style.backgroundColor = "white";
+    yearlyPlanPro.style.border = " solid 1px hsl(231, 11%, 63%)";
+    /* la suite */
+    //document.getElementById("modeResume").innerHTML = "Arcade (monthly)";
+    //document.getElementById("priceResume").innerHTML = "9$/mo";
+    //document.getElementById("modeTotal").innerHTML = "Total (per month)";
+});
+
+/* YEARLY Choosing a plan / changing colors */
+checkBox = document.getElementById("yearlyPlanArc").addEventListener("click", event => {
+    yearlyPlanArc.style.backgroundColor = "hsl(217, 100%, 97%)";
+    yearlyPlanArc.style.border = " solid 1px hsl(243, 100%, 62%)";
+    /*res les autres background et borders */
+    yearlyPlanAdv.style.backgroundColor = "white";
+    yearlyPlanAdv.style.border = " solid 1px hsl(231, 11%, 63%)";
+    yearlyPlanPro.style.backgroundColor = "white";
+    yearlyPlanPro.style.border = " solid 1px hsl(231, 11%, 63%)";
+    monthlyPlanArc.style.backgroundColor = "white";
+    monthlyPlanArc.style.border = " solid 1px hsl(231, 11%, 63%)";
+    monthlyPlanAdv.style.backgroundColor = "white";
+    monthlyPlanAdv.style.border = " solid 1px hsl(231, 11%, 63%)";
+    monthlyPlanPro.style.backgroundColor = "white";
+    monthlyPlanPro.style.border = " solid 1px hsl(231, 11%, 63%)";
+    /* la suite */
+    //document.getElementById("modeResume").innerHTML = "Arcade (monthly)";
+    //document.getElementById("priceResume").innerHTML = "9$/mo";
+    //document.getElementById("modeTotal").innerHTML = "Total (per month)";
+});
+checkBox = document.getElementById("yearlyPlanAdv").addEventListener("click", event => {
+    yearlyPlanAdv.style.backgroundColor = "hsl(217, 100%, 97%)";
+    yearlyPlanAdv.style.border = " solid 1px hsl(243, 100%, 62%)";
+    /*res les autres background et borders */
+    yearlyPlanArc.style.backgroundColor = "white";
+    yearlyPlanArc.style.border = " solid 1px hsl(231, 11%, 63%)";
+    yearlyPlanPro.style.backgroundColor = "white";
+    yearlyPlanPro.style.border = " solid 1px hsl(231, 11%, 63%)";
+    monthlyPlanArc.style.backgroundColor = "white";
+    monthlyPlanArc.style.border = " solid 1px hsl(231, 11%, 63%)";
+    monthlyPlanAdv.style.backgroundColor = "white";
+    monthlyPlanAdv.style.border = " solid 1px hsl(231, 11%, 63%)";
+    monthlyPlanPro.style.backgroundColor = "white";
+    monthlyPlanPro.style.border = " solid 1px hsl(231, 11%, 63%)";
+    /* la suite */
+    //document.getElementById("modeResume").innerHTML = "Arcade (monthly)";
+    //document.getElementById("priceResume").innerHTML = "9$/mo";
+    //document.getElementById("modeTotal").innerHTML = "Total (per month)";
+});
+checkBox = document.getElementById("yearlyPlanPro").addEventListener("click", event => {
+    yearlyPlanPro.style.backgroundColor = "hsl(217, 100%, 97%)";
+    yearlyPlanPro.style.border = " solid 1px hsl(243, 100%, 62%)";
+    /*res les autres background et borders */
+    yearlyPlanAdv.style.backgroundColor = "white";
+    yearlyPlanAdv.style.border = " solid 1px hsl(231, 11%, 63%)";
+    yearlyPlanArc.style.backgroundColor = "white";
+    yearlyPlanArc.style.border = " solid 1px hsl(231, 11%, 63%)";
+    monthlyPlanArc.style.backgroundColor = "white";
+    monthlyPlanArc.style.border = " solid 1px hsl(231, 11%, 63%)";
+    monthlyPlanAdv.style.backgroundColor = "white";
+    monthlyPlanAdv.style.border = " solid 1px hsl(231, 11%, 63%)";
+    monthlyPlanPro.style.backgroundColor = "white";
+    monthlyPlanPro.style.border = " solid 1px hsl(231, 11%, 63%)";
+    /* la suite */
+    //document.getElementById("modeResume").innerHTML = "Arcade (monthly)";
+    //document.getElementById("priceResume").innerHTML = "9$/mo";
+    //document.getElementById("modeTotal").innerHTML = "Total (per month)";
 });
